ce1a7a3a1f8e37143c75bc468da2f3c2f8bfc235
Charisma <actuallycharisma@outlook.com>
2024-06-05 17:15:53 +0100

    completed booking service class

--- start_point/src/main/java/com/example/airline_api/services/BookingService.java
+++ start_point/src/main/java/com/example/airline_api/services/BookingService.java
@@ -1,8 +1,15 @@
 package com.example.airline_api.services;
 
+import com.example.airline_api.models.Booking;
+import com.example.airline_api.models.BookingDTO;
+import com.example.airline_api.models.Flight;
+import com.example.airline_api.models.Passenger;
 import com.example.airline_api.repositories.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookingService {
@@ -16,6 +23,25 @@ public class BookingService {
     @Autowired
     FlightService flightService;
 
+    public List<Booking> findAllBookings(){
+        return bookingRepository.findAll();
+    }
+
+    public Optional<Booking> findBooking(long id){
+        return bookingRepository.findById(id);
+    }
+
+    public Booking saveBooking(BookingDTO bookingDTO){
+        Passenger passenger = passengerService.findSinglePassenger(bookingDTO.getPassengerId()).get();
+        Flight flight = flightService.findSingleFlight(bookingDTO.getFlightId()).get();
+        Booking booking = new Booking(
+                flight,
+                passenger,
+                bookingDTO.getSeatNumber()
+        );
+        flight.setCapacity(flight.getCapacity() - 1);
+        return booking;
+    }
 
 
 }
